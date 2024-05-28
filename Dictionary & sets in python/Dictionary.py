8b150eb19837773a0286358b8d5ad3a4c0af44f8
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-28 19:03:21 +0530

    Add

--- Dictionary & sets in python/Dictionary.py
+++ Dictionary & sets in python/Dictionary.py
@@ -1,9 +1,28 @@
 
-info = {
-    "name" : "Aditya",
-    "cgpa" : 9.8,
-    "marks" : [89,90,88],
-    "subject" : ("python", "C", "Java"),
+student = {
+    "Name" : "Aditya",
+    "subjects" : {
+        "C++" : 90,
+        "Maths" : 85,
+        "English" : 90,
+    }
 }
+# nested dictionary
+# print(student["subjects"]["Maths"])
 
-print(len(info))
+# return all keys
+# print(list(student.keys()))
+
+# return all values
+# print(student.values())
+
+# return all (key,val) pairs as tuples
+# print(student.items())
+
+# return the key according to value
+# print(student.keys())
+
+# insert the specified items to the dictionary
+student.update({"city" : "dehardun"})
+
+print(student)
--- Dictionary & sets in python/readme.md
+++ Dictionary & sets in python/readme.md
@@ -3,5 +3,9 @@
 > - Dictionaries are used to store data value in key:value pairs. <br> 
 > * They are unordered, mutable(changeable) & don't allow duplicate keys <br> <br>
 ![Syntax](image.png) <br>
-![Nested](image-1.png)
+![Nested](image-1.png) <br>
+![Methods](image-2.png)
+
+# Set in Python
+![Set](image-3.png)
 
