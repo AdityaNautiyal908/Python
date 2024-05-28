5f00b5a6a07d448e92df5ad8b625aaac769fbb3b
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-28 07:34:41 +0530

    Add

---
+++ first.py
@@ -0,0 +1,28 @@
+
+# function in python
+
+# return true if ends with str.(er)
+
+# str = "I am Monkey D Luffy am the am thee am"
+# print(str.endswith("er"))
+
+# print(str.capitalize())
+
+# print(str.replace("Monkey","Aditya"))
+
+# print(str.find("Luffy"))
+
+# print(str.count("am"))
+
+
+# Q1 first print lenght of your name
+# name = input("Enter your name : ")
+# print(len(name))
+
+
+# dollar = input("Enter dollar : ")
+# print(dollar.count("$"))
+
+
+
+
