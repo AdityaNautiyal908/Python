97064822e4291e12c82fbad01d68363746258e02
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-28 08:45:23 +0530

    Add new files

---
+++ Condition.py
@@ -0,0 +1,79 @@
+
+# Condition Statement in Python
+
+# light = input("Enter the light color : ")
+
+# if(light == "red"):
+#     print("Stop")
+
+# elif(light == "yellow"):
+#     print("Ready")
+
+# else:
+#     print("Go")
+
+
+# marks = int (input("Enter your marks : "))
+
+# if(marks >= 90):
+#     print("A")
+
+# elif(marks < 90 and marks >= 80):
+#     print("B")
+
+# elif(marks < 80 and marks >= 70):
+#     print("C")
+
+# else:
+#     print("D")
+
+
+# WAP to check if number is odd or even
+
+# num =  int (input("Enter the number : "))
+
+# if(num % 2 == 0):
+#     print("Number is Even",num)
+
+# else:
+#     print("Number is Odd",num)
+
+# WAP to find the greatest of 3 number entered by the user
+
+# num1 = int (input("Enter the first number : "))
+# num2 = int (input("Enter the first number : "))
+# num3 = int (input("Enter the first number : "))
+
+# if(num1 >= num2 and num1 >= num3):
+#     print("Number1 is greater",num1)
+
+# elif(num2 >= num1 and num2 >= num3):
+#     print("Number2 is greater",num2)
+
+# else:
+#     print("Number3 is greater",num3)
+
+# WAP to find the greatest of 4 number entered by the user
+
+# a = int (input("Enter the first number : "))
+# b = int (input("Enter the first number : "))
+# c = int (input("Enter the first number : "))
+# d = int (input("Enter the first number : "))
+
+# if(a >= b and a >= c and a >= d):
+#     print("First is greater number ",a)
+# elif(b >= c and b >= d):
+#     print("Second is greater number ",b)
+# elif(c >= b and c>=d):
+#     print("Third is greater number ",c)
+# else:
+#     print("Fourth is greater number ",d)
+
+# WAP to check if a number is multiple of 7 or not
+
+num = int (input("Enter the number : "))
+
+if(num % 7 == 0):
+    print(num,"is multiple of 7")
+else:
+    print(num,"Not a multiple of 7")
