cbd9a1b2eabfc9dfa85da71efe7adc7d28ce3ae6
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-10-09 18:11:30 +0530

    add

--- OPPS/1.py
+++ OPPS/1.py
@@ -1,1 +1,15 @@
-class 
+class Student:
+    name = "Aditya Nautiyal"
+
+s1 = Student()
+print(s1.name)
+
+
+# class Car:
+#     color = "Red"
+#     brand = "mercedes"
+
+
+# car1 = Car()
+# print(car1.color)
+# print(car1.brand)
