a6e554593db43f43cab1eca2ed0fb803787f9943
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-10-31 09:49:31 +0530

    add

--- Project1/emailty.py
+++ Project1/emailty.py
@@ -1,4 +1,14 @@
-num1 = int(input('Enter the first number : '))
-num2 = int(input('Enter the second number : '))
+# a-z (small case character) nautiyaladitya7@gmail.com
+# 0-9 (digit)
+# . _ time 1
+# @ count only 1
+# . 2, 3 position
 
-print((lambda x,y:x+y)(num1,num2))
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_input = input('Enter your email : ')
+
+if re.search(email_condition,user_input):
+    print(f'{user_input} is a valid email address')
+else:
+    print(f'{user_input} is a invalid email address')
